4ee74a532a0d17650ce6a46894a55818f345b170
yano0 <yanonay3@gmail.com>
2022-09-23 03:15:05 +0900

    irokae

--- src/Tabpanel.jsx
+++ src/Tabpanel.jsx
@@ -16,6 +16,16 @@ import hate from "./keyword/hate";
 import internet from "./keyword/internet";
 import life from "./keyword/life";
 import Link from "@mui/material/Link";
+import { createTheme } from '@mui/material/styles'
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+    palette: {
+        secondary: {
+            main: "#ffa600"
+        }
+    }
+});
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -66,141 +76,143 @@ export default function BasicTabs() {
     };
 
     return (
-        <Box sx={{ width: "100%", backgroundColor: "#f0f0f0", height: "100vh" }}>
-            <Box
-                sx={{
-                    color: "#fff",
-                    pt: 5,
-                    backgroundColor: "#ffa600",
-                }}
-            >
-                <Typography
-                    variant="h3"
-                    marginLeft={15}
-                    color="#fff"
-                    mb={5}
-                    fontWeight="bold"
+        <ThemeProvider theme={theme}>
+            <Box sx={{ width: "100%", backgroundColor: "#f0f0f0", height: "100vh" }}>
+                <Box
+                    sx={{
+                        color: "#fff",
+                        pt: 5,
+                        backgroundColor: "#5b6b95",
+                    }}
                 >
-                    わたしを表す101キーワード
-                </Typography>
-
-                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        centered
-                        textColor="inherit"
-                        indicatorColor="secondary"
+                    <Typography
+                        variant="h3"
+                        marginLeft={15}
+                        color="#fff"
+                        mb={5}
+                        fontWeight="bold"
                     >
-                        <Tab label="お絵描き" {...a11yProps(0)} />
-                        <Tab label="音楽" {...a11yProps(1)} />
-                        <Tab label="動画" {...a11yProps(2)} />
-                        <Tab label="ゲーム" {...a11yProps(3)} />
-                        <Tab label="ファッション" {...a11yProps(4)} />
-                        <Tab label="本" {...a11yProps(5)} />
-                        <Tab label="生活" {...a11yProps(6)} />
-                        <Tab label="性格" {...a11yProps(7)} />
-                        <Tab label="嫌い" {...a11yProps(8)} />
-                        <Tab label="頑張っていること" {...a11yProps(9)} />
-                    </Tabs>
+                        わたしを表す101キーワード
+                    </Typography>
+
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            centered
+                            textColor="inherit"
+                            indicatorColor="secondary"
+                        >
+                            <Tab label="お絵描き" {...a11yProps(0)} />
+                            <Tab label="音楽" {...a11yProps(1)} />
+                            <Tab label="動画" {...a11yProps(2)} />
+                            <Tab label="ゲーム" {...a11yProps(3)} />
+                            <Tab label="ファッション" {...a11yProps(4)} />
+                            <Tab label="本" {...a11yProps(5)} />
+                            <Tab label="生活" {...a11yProps(6)} />
+                            <Tab label="性格" {...a11yProps(7)} />
+                            <Tab label="嫌い" {...a11yProps(8)} />
+                            <Tab label="頑張っていること" {...a11yProps(9)} />
+                        </Tabs>
+                    </Box>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <ul style={{ columnCount: 2, listStyle: "square" }}>
+                        {draw.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <ul style={{ listStyle: "square" }}>
+                        {music.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    <ul style={{ listStyle: "square" }}>
+                        {internet.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                    <ul style={{ listStyle: "square" }}>
+                        {game.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={4}>
+                    <ul style={{ listStyle: "square" }}>
+                        {fashion.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={5}>
+                    <ul style={{ listStyle: "square" }}>
+                        {book.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={6}>
+                    <ul style={{ listStyle: "square" }}>
+                        {life.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={7}>
+                    <ul style={{ listStyle: "square" }}>
+                        {character.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={8}>
+                    <ul style={{ listStyle: "square" }}>
+                        {hate.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <TabPanel value={value} index={9}>
+                    <ul style={{ columnCount: 2, listStyle: "square" }}>
+                        {effort.map((element, i) => (
+                            <li key={i}>{element}</li>
+                        ))}
+                    </ul>
+                </TabPanel>
+                <Box
+                    sx={{
+                        width: "100%",
+                        backgroundColor: "#5b6b95",
+                        bottom: 0,
+                        position: "fixed",
+                        color: "#fff",
+                        p: 2,
+                        textAlign: "center",
+                    }}
+                >
+                    <Link
+                        href="https://github.com/yano0"
+                        color="#ffa600"
+                        marginRight={10}
+                    >
+                        GitHub
+                    </Link>
+
+                    <Link
+                        href="https://sites.google.com/view/chihiroyano"
+                        color="#ffa600"
+                    >
+                        portfolio
+                    </Link>
                 </Box>
             </Box>
-            <TabPanel value={value} index={0}>
-                <ul style={{ columnCount: 2, listStyle: "square" }}>
-                    {draw.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <ul style={{ listStyle: "square" }}>
-                    {music.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                <ul style={{ listStyle: "square" }}>
-                    {internet.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                <ul style={{ listStyle: "square" }}>
-                    {game.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={4}>
-                <ul style={{ listStyle: "square" }}>
-                    {fashion.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={5}>
-                <ul style={{ listStyle: "square" }}>
-                    {book.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={6}>
-                <ul style={{ listStyle: "square" }}>
-                    {life.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={7}>
-                <ul style={{ listStyle: "square" }}>
-                    {character.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={8}>
-                <ul style={{ listStyle: "square" }}>
-                    {hate.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <TabPanel value={value} index={9}>
-                <ul style={{ columnCount: 2, listStyle: "square" }}>
-                    {effort.map((element, i) => (
-                        <li key={i}>{element}</li>
-                    ))}
-                </ul>
-            </TabPanel>
-            <Box
-                sx={{
-                    width: "100%",
-                    backgroundColor: "#ffa600",
-                    bottom: 0,
-                    position: "fixed",
-                    color: "#fff",
-                    p: 2,
-                    textAlign: "center",
-                }}
-            >
-                <Link
-                    href="https://github.com/yano0"
-                    color="secondary"
-                    marginRight={10}
-                >
-                    GitHub
-                </Link>
-
-                <Link
-                    href="https://sites.google.com/view/chihiroyano"
-                    color="secondary"
-                >
-                    portfolio
-                </Link>
-            </Box>
-        </Box>
+        </ThemeProvider>
     );
 }
