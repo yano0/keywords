739463528145cc00072f6ebb0f1127328f418729
yano0 <yanonay3@gmail.com>
2022-09-22 11:40:51 +0900

    moromoro

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/keywords" element={<App />} />
+        <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
